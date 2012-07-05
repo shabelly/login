096d7e3f0f32d4c7da8e8e96d86569202a9250dd
Proyecto_Ubuntu <evelynadaro@gmail.com>
2012-07-05 14:12:30 -0700

    Se agrego la opción de roles a la creacion de users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,16 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def index
+    @users = User.all
 
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @cargos }
+    end
+  end
+  
   def create
     @user = User.new(params[:user])
     if @user.save
